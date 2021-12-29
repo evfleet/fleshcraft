3d2c33c7ea3caa3fa8d8048f507d0c2842d778f1
Evan Fleet <evfleet@gmail.com>
2021-12-29 17:02:19 -0500

    feat(api): setup winston and configure morgan to use it

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,20 +1,21 @@
 import express from "express";
-import morgan from "morgan";
+import logger from "./config/logger";
+import morgan from "./config/morgan";
 
 const PORT = 42069;
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan);
 
 app.get("*", (req, res) => {
-	res.status(404).json({
-		status: 404,
-		message: "This isn't where I parked my car.",
-	});
+  res.status(404).json({
+    status: 404,
+    message: "This isn't where I parked my car.",
+  });
 });
 
 app.listen(PORT, () => {
-	console.info("🌍 Listening on port " + PORT);
+  logger.info("🌍 Listening on port " + PORT);
 });
